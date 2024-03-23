e6e497aa60886d8c43fd0dfc9ebaa196f5a066c7
Karmanya <karmanyasingh8@gmail.com>
2024-03-23 18:57:45 +0530

    service page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram, faXTwitter } from '@fortawesome/free-brands-svg-icons';
 import Navbar from "./Components/Navbar"; 
+import ServicesSkills from "./Components/Services";
 import "./App.css"; 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <p>At Nimbus Web , we combine technical expertise with creativity to build websites and web applications that not only look great but also perform exceptionally well.</p>
         <p>Whether you need a simple business website or a complex e-commerce platform, we have the skills and experience to bring your vision to life.</p>
       </div>
+      <ServicesSkills />
       <footer className="footer">
         <a href="https://www.instagram.com/nimbuswebdev/"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
         <a href="https://twitter.com"><FontAwesomeIcon icon={faXTwitter} size="2x" /></a>
